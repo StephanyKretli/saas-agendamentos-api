8ae4a80527bb05bcd5ff028da2f15906e717eff2
Stephany Kretli <stephanykretli@gmail.com>
2026-05-30 22:33:19 -0300

    Add EmailModule to SupportModule imports

--- src/modules/support/support.module.ts
+++ src/modules/support/support.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupportController } from './support.controller';
 import { SupportService } from './support.service';
-import { PrismaModule } from '../../prisma/prisma.module'; // 👈 Verifique se o caminho para o seu PrismaModule está correto
+import { PrismaModule } from '../../prisma/prisma.module'; 
+import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EmailModule],
   controllers: [SupportController],
   providers: [SupportService],
 })

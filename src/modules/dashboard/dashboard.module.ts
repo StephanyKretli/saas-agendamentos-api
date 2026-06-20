1f083f70e4ab753aa163103597156fc24ff3ceaf
Stephany Kretli <stephanykretli@gmail.com>
2026-06-19 22:53:04 -0300

    fix: importa PaymentsModule no TeamModule

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { PaymentsModule } from '../payments/payments.module'; 
+
 
 @Module({
   controllers: [DashboardController],
   providers: [DashboardService, PrismaService],
+  imports: [PaymentsModule],
 })
 export class DashboardModule {}
--- src/modules/team/team.module.ts
+++ src/modules/team/team.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import TeamController from './team.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { PaymentsModule } from '../payments/payments.module'; 
 
 @Module({
   controllers: [TeamController],
   providers: [TeamService, PrismaService],
   exports: [TeamService],
+  imports: [PaymentsModule]
 })
 export class TeamModule {}

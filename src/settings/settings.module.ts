b5d46137e3c213636b9a3352d3a2456cea5e9b9a
Stephany Kretli <stephanykretli@gmail.com>
2026-06-19 23:15:14 -0300

    fix: importa PaymentsModule no SettingsModule e restantes

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -3,9 +3,10 @@ import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UploadsModule } from '../modules/uploads/uploads.module';
+import { PaymentsModule } from '../modules/payments/payments.module';
 
 @Module({
-  imports: [PrismaModule, UploadsModule],
+  imports: [PrismaModule, UploadsModule, PaymentsModule],
   controllers: [SettingsController],
   providers: [SettingsService],
 })

b650d10142159e1f5ca0455a85cb1156fab012f9
Stephany Kretli <stephanykretli@gmail.com>
2026-06-19 23:03:06 -0300

    fix: importa PaymentsModule nos modulos dependentes do SubscriptionGuard

--- src/modules/services/services.module.ts
+++ src/modules/services/services.module.ts
@@ -3,9 +3,9 @@ import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { UploadsModule } from '../uploads/uploads.module';
-
+import { PaymentsModule } from '../payments/payments.module';
 @Module({
-  imports: [PrismaModule, UploadsModule],
+  imports: [PrismaModule, UploadsModule, PaymentsModule],
   providers: [ServicesService],
   controllers: [ServicesController],
 })
